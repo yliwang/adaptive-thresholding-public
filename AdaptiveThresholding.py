fea8d4098ffe4a4f7006f612bef55f17eb0cbdb0
yliwang <1220264221@qq.com>
2021-11-17 16:01:17 +0800

    Create AdaptiveThresholding.py

---
+++ AdaptiveThresholding.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def NiblackThresholding( f, # 2d numpy array representing an image
+                         n ): # n is the window size for neighorhood, n is odd
+                            # for example n = 3, means a 3x3 neighborhood
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    
